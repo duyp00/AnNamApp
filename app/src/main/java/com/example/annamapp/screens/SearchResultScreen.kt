215be07c6d2b988a94ef4783c8bc38d50b830564
duyp00 <135790151+duyp00@users.noreply.github.com>
2025-11-24 20:30:47 +0700

    commit before enabling staging area feature to see what happen

--- app/src/main/java/com/example/annamapp/screens/SearchResultScreen.kt
+++ app/src/main/java/com/example/annamapp/screens/SearchResultScreen.kt
@@ -1,6 +1,5 @@
 package com.example.annamapp.screens
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
