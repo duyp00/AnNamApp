6499e5c00c280d8e13b11a8d5b2de1afdaf55006
duyp00 <135790151+duyp00@users.noreply.github.com>
2025-11-29 21:17:46 +0700

    annoying indent

--- app/src/main/java/com/example/annamapp/room_sqlite_db/FlashCardEntity.kt
+++ app/src/main/java/com/example/annamapp/room_sqlite_db/FlashCardEntity.kt
@@ -5,11 +5,12 @@ import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "FlashCards", indices = [Index(
-    value = ["english_card", "vietnamese_card"],
-    unique = true
-)])
-data class FlashCard(
+@Entity(
+    tableName = "FlashCards",
+    indices = [Index(value = ["english_card", "vietnamese_card"],
+                     unique = true)
+              ]
+) data class FlashCard(
     @PrimaryKey(autoGenerate = true) val uid: Int,
     @ColumnInfo(name = "english_card") val englishCard: String?,
     @ColumnInfo(name = "vietnamese_card") val vietnameseCard: String?
